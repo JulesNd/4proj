0e3874371b4bc7dffd42c858c324f59c4e06d1c0
JulesxD <julesfran@outlook.fr>
2021-09-23 16:13:43 +0200

    commit

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+class Html5QrcodeScanner {
+  constructor(reader: string, param2: { qrbox: number; fps: number }) {
+  }
+
+  render(onScanSuccess: (decodedText, decodedResult) => void) {
+  }
+}
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -10,6 +18,15 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function onScanSuccess(decodedText, decodedResult) {
+      // Handle on success condition with the decoded text or result.
+      console.log(`Scan result: ${decodedText}`, decodedResult);
+    }
+
+    // tslint:disable-next-line:prefer-const
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+      'reader', { fps: 10, qrbox: 250 });
+    html5QrcodeScanner.render(onScanSuccess);
   }
 
 }
